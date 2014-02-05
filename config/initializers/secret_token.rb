dcd7e5a1613b6544c3b87ef9ba23ede5c7436863
Johannes Kananen <jkananen@gmail.com>
2014-02-05 20:34:27 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmaUus::Application.config.secret_key_base = 'd0f281cd0e5677c807a102932e6cfc791c7d5f453ead87ef25fcb7c9573b34425f661a6caa4a62d60c0cd7149f92940b1f0453379b2c23bc926448a0737b1b1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmaUus::Application.config.session_store :cookie_store, key: '_oma uus_session'
